690eeb22f595936619245adc2dbb64384f30c064
Qing07430 <158696189+Qing07430@users.noreply.github.com>
2025-03-05 21:29:08 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,21 +1,24 @@
 import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from torch.utils.data import random_split, DataLoader, Subset
+from torch.utils.data import Subset
 
 
-def get_mnist_loaders(batch_size=1):
+def get_mnist_loaders():
     transform = transforms.ToTensor()
+
     train_dataset = datasets.MNIST(root='data', train=True, download=True, transform=transform)
     test_dataset = datasets.MNIST(root='data', train=False, download=True, transform=transform)
 
-    # 裁剪数据，随机选取20%的数据
-    train_size = int(0.1 * len(train_dataset))  # 20%
-    train_indices = torch.randperm(len(train_dataset))[:train_size]
-    train_subset = Subset(train_dataset, train_indices)
+    # 只使用10%的数据
+    train_size = int(0.2 * len(train_dataset))
+    test_size = int(0.2 * len(test_dataset))
 
-    test_size = int(0.1 * len(test_dataset))
+    train_indices = torch.randperm(len(train_dataset))[:train_size]
     test_indices = torch.randperm(len(test_dataset))[:test_size]
+
+    train_subset = Subset(train_dataset, train_indices)
     test_subset = Subset(test_dataset, test_indices)
 
     return train_subset, test_subset
+
